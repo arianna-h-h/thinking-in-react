ef5071d16271b3e445e9d00e112b96576bf74909
arianna-h-h <ariannaharadon@gmail.com>
2017-08-22 10:49:20 -0500

    Refactor from hard code to stateful

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 class ProductCategoryRow extends React.Component {
   render() {
     return <tr><th colSpan="2">{this.props.category}</th></tr>;
@@ -29,8 +28,13 @@ class ProductTable extends React.Component {
   render() {
     var rows = [];
     var lastCategory = null;
-    this.props.products.forEach(function(product) {
-      if (product.category !== lastCategory) { // if product's cat doesn't equal the former one, make a new cat
+    console.log(this.props.inStockOnly);
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(this.props.filterText) === -1 ||
+       (!product.stocked && this.props.inStockOnly)) {
+        return;
+      } // if product's cat doesn't equal the former one, make a new cat
+      if (product.category !== lastCategory){
         rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
       } //else push the next product
       rows.push(<ProductRow product={product} key={product.name} />);
@@ -51,12 +55,35 @@ class ProductTable extends React.Component {
 }
 
 class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleFilterTextInputChange = //handleFilterTextInputChange will refer to SearchBar
+    this.handleFilterTextInputChange.bind(this);
+    this.handleInStockInputChange =
+    this.handleInStockInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+
+  handleInStockInputChange(e) {
+    this.props.onInStockInput(e.target.checked);
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Search..." />
+        <input type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextInputChange}
+        />
         <p>
-          <input type="checkbox" />
+          <input type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockInputChange}
+          />
           {' '}
           Only show products in stock
         </p>
@@ -66,11 +93,45 @@ class SearchBar extends React.Component {
 }
 
 class FilterableProductTable extends React.Component {
+  constructor(props){ // props passed in is product array
+    super(props); // product array
+    this.state = { // set initial state
+      filterText: '',
+      inStockOnly: false
+    };
+
+    this.handleFilterTextInput = //makes this always be the current this
+  this.handleFilterTextInput.bind(this);
+    this.handleInStockInput =
+  this.handleInStockInput.bind(this);
+  }
+
+  handleFilterTextInput(filterText){ //handler function that updates state
+    this.setState({
+      filterText: filterText
+    });
+  }
+
+  handleInStockInput(inStockOnly){ //passes in state of inStock check box
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextInput={this.handleFilterTextInput}
+          onInStockInput={this.handleInStockInput} //triggered on change
+        />
+        <ProductTable
+          products={this.props.products} //props passed into product table
+          filterText={this.state.filterText} //initial/default state when 1st created
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
